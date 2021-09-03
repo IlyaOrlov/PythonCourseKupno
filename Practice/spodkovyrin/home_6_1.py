a199f284a358388529dea4a015069f05b2b8d739
GeP-Nuie <GeP-Nuie@users.noreply.github.com>
2021-09-03 13:37:41 +0300

    Исправил ошибки с именами переменных файлов

--- Practice/spodkovyrin/home_6_1.py
+++ Practice/spodkovyrin/home_6_1.py
@@ -7,8 +7,8 @@ with open('text.txt', 'r') as f, open('text_tab.txt', 'w') as f_tab:
         print(line_w_tabs)
         f_tab.write(line_w_tabs)
 
-with open('text_tab.txt', 'r') as f, open('text_space.txt', 'w') as f_space:
-    for line in f:
+with open('text_tab.txt', 'r') as f_tab, open('text_space.txt', 'w') as f_space:
+    for line in f_tab:
         line_w_space = line.replace('\t', '    ')
         print(line_w_space)
         f_space.write(line_w_space)

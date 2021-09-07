8d7af1957cbf92c66c716475ec2738431375b9d6
GeP-Nuie <send.red@mail.ru>
2021-09-07 18:09:22 +0300

    Исправил функцию удаления

--- Practice/spodkovyrin/home_6_3.py
+++ Practice/spodkovyrin/home_6_3.py
@@ -1,15 +1,23 @@
 import random
 
 def del_column(lst):
-    x = int(input('Введите число: '))
-    for j in reversed(range(column)):
-        for i in range(row):
+    for i in range(len(lst)):
+        for j in reversed(range(len(lst[i]))):
             if lst[i][j] == x:
-                for k in range(row):
+                for k in range(len(lst)):
                     del lst[k][j]
+
+# Закоментирована проверка статичной матрицы
+# lst = [
+#     [1, 1, 2],
+#     [1, 2, 1],
+#     [1, 1, 1]
+# ]
+
 row = int(input('Введите кол-во строк: '))
 column = int(input('Введите кол-во столбцов: '))
 lst = [[random.randrange(1, 10) for _ in range(column)] for _ in range(row)]
 print(*lst, sep='\n')
+x = int(input('Введите число: '))
 del_column(lst)
 print(*lst, sep='\n')

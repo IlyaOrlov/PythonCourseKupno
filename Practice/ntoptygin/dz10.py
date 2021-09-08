dcc825c57466c155bbcddcb97520c7756c6e0bc7
Nikita Toptygin <topnikita@mail.ru>
2021-09-08 11:05:53 +0300

    Подправил ДЗ 10

--- Practice/ntoptygin/dz10.py
+++ Practice/ntoptygin/dz10.py
@@ -3,28 +3,29 @@
 # табуляции на четыре пробела, либо же заменить все комбинации из четырех символов пробела на
 # символ табуляции
 
+import os
+
 with open('text.txt', 'w') as f:
     f.write(input('Введите текст '))
 
 a = None
 
-while not a == 'space' or 'tab':
-    a = input('Введите, что Вы хотите сделать. Если заменить табуляцию на пробелы - нажмите tab. Если заменить пробелы на табуляцию - space. ')
-    if a == 'space':
-        with open('text.txt', 'r+') as f:
-            for line in f:
+while not (a == 'space' or a == 'tab' ):
+    a = input('Введите, что Вы хотите сделать. Если заменить табуляцию на пробелы - нажмите tab. \
+Если заменить пробелы на табуляцию - space. ')
+    with open('text.txt', 'r') as f, \
+             open('new_text.txt', 'w') as nf:
+        for line in f:
+            if a == 'space':
                 line = line.replace('    ', '\t')
-                print(line)
-                f.write(line)
-        break
-
-    elif a == 'tab':
-        with open('text.txt', 'r+') as f:
-            for line in f:
+            elif a == 'tab':
                 line = line.replace('\t', '    ')
-                print(line)
-                f.write(line)
-        break
+            else:
+                print('Нужно было ввести space или tab', end='')
+            print(line)
+            nf.write(line)
 
-    else:
-        print('Нужно ввести space или tab')
+
+
+os.remove('text.txt')
+os.rename('new_text.txt', 'text.txt')

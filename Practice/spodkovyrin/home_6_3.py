3cb5795c9d95b9cba7d51becfe9022fc37612b96
GeP-Nuie <GeP-Nuie@users.noreply.github.com>
2021-09-08 11:43:38 +0300

    Добавил x как аргумент

--- Practice/spodkovyrin/home_6_3.py
+++ Practice/spodkovyrin/home_6_3.py
@@ -1,23 +1,27 @@
 import random
 
-def del_column(lst):
+def del_column(lst, x):
     for i in range(len(lst)):
         for j in reversed(range(len(lst[i]))):
             if lst[i][j] == x:
                 for k in range(len(lst)):
                     del lst[k][j]
 
-# Закоментирована проверка статичной матрицы
-# lst = [
-#     [1, 1, 2],
-#     [1, 2, 1],
-#     [1, 1, 1]
-# ]
+lst = [
+    [1, 1, 2],
+    [1, 2, 1],
+    [1, 1, 1]
+]
+
+print(*lst, sep='\n')
+x = int(input('Введите число: '))
+del_column(lst, x)
+print(*lst, sep='\n')
 
 row = int(input('Введите кол-во строк: '))
 column = int(input('Введите кол-во столбцов: '))
-lst = [[random.randrange(1, 10) for _ in range(column)] for _ in range(row)]
-print(*lst, sep='\n')
+lst1 = [[random.randrange(1, 10) for _ in range(column)] for _ in range(row)]
+print(*lst1, sep='\n')
 x = int(input('Введите число: '))
-del_column(lst)
-print(*lst, sep='\n')
+del_column(lst1, x)
+print(*lst1, sep='\n')

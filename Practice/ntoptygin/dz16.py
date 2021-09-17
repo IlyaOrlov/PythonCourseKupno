16a099d175d99245dfd5a59366193b2cbdb6adfc
Nikita Toptygin <topnikita@mail.ru>
2021-09-17 17:58:15 +0300

    Сделал задания

---
+++ Practice/ntoptygin/dz16.py
@@ -0,0 +1,16 @@
+# В чем ошибка в данной программе и как ее можно исправить?
+
+# def chargen():
+#     while True:
+#         for c in '0123456789':
+#             yield c
+# words = [c+c for c in chargen()][:10]
+
+def chargen():
+        for c in '0123456789':
+            yield c
+for c in chargen():
+    words = [c+c for c in chargen()][:10]
+print(words)
+
+# проблема была в том, что значение C из функции chargen не передавалось в массив Words

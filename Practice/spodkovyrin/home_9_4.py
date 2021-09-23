12bfb6ca4042306431ee14de3798f0c229bf8261
GeP-Nuie <GeP-Nuie@users.noreply.github.com>
2021-09-23 16:25:50 +0300

    Оно заработало!!!!

--- Practice/spodkovyrin/home_9_4.py
+++ Practice/spodkovyrin/home_9_4.py
@@ -4,17 +4,27 @@ import shutil
 
 
 def remove_folder(path):
-    if not shutil.rmtree(path):
+    # if not shutil.rmtree(path):
+    deleted_folders_count = 0
+    if len(os.listdir(path)) == 0:
+        shutil.rmtree(path)
         print(f'{path} Успешно удален')
+        deleted_folders_count += 1
+        print(f'Папок удалено: {deleted_folders_count}')
     else:
-        print(f'Невозможно удалить {path}')
+        pass
 
 
 def remove_file(path):
-    if not os.remove(path):
+    # if not os.remove(path):
+    deleted_files_count = 0
+    if os.path.isfile(path):
+        os.remove(path)
         print(f"{path} Успешно удален")
+        deleted_files_count += 1
+        print(f'Файлов удалено: {deleted_files_count}')
     else:
-        print(f"Невозможно удалить {path}")
+        pass
 
 
 def get_age(path):
@@ -22,37 +32,36 @@ def get_age(path):
     return ctime
 
 
-deleted_folders_count = 0
-deleted_files_count = 0
-
 path = r'E:\Study\PythonCourseKupno\Practice\spodkovyrin\work_test'
 time_folders = 2
 time_files = 1
 
-seconds_folders = time.time() - (time_folders * 60)
-seconds_files = time.time() - (time_files * 60)
 
 while True:
+    seconds_folders = time.time() - (time_folders * 60)
+    seconds_files = time.time() - (time_files * 60)
+
     if os.path.exists(path):
         for root_folder, folders, files in os.walk(path):
             for folder in folders:
                 folder_path = os.path.join(root_folder, folder)
                 if seconds_folders >= get_age(folder_path):
                     remove_folder(folder_path)
-                    deleted_folders_count += 1
-                    print(f'Папок удалено: {deleted_folders_count}')
+                else:
+                    for folder in folders:
+                        folder_path = os.path.join(root_folder, folder)
+                        if seconds_folders >= get_age(folder_path):
+                            remove_folder(folder_path)
+                    for file in files:
+                        file_path = os.path.join(root_folder, file)
+                        if seconds_files >= get_age(file_path):
+                            remove_file(file_path)
             for file in files:
                 file_path = os.path.join(root_folder, file)
                 if seconds_files >= get_age(file_path):
                     remove_file(file_path)
-                    deleted_files_count += 1
-                    print(f'Файлов удалено: {deleted_files_count}')
         else:
             if seconds_files >= get_age(path):
                 remove_file(path)
-                deleted_files_count += 1
-                print(f'Файлов удалено: {deleted_files_count}')
     else:
         print(f'"{path}" is not found')
-        deleted_files_count += 1
-        print(f'Файлов удалено: {deleted_files_count}')

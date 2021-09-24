205b3caf88adc59c37447ffa1280dd4f232497c7
m.gavrilova <marya.gama1@yandex.ru>
2021-09-24 18:28:29 +0300

    экзамен 4

---
+++ Practice/gavrilova/5.py
@@ -0,0 +1,7 @@
+def count_symbol(a):
+    counter = a.count('i')
+    return counter
+
+s = 'Hi, Elvis, I am here!'
+s1 = count_symbol(s)
+print ('Символ i встречается в строке: ' + s + ' '+str(s1)+' раза')

8da7a1b8327cc3d67ce54e8ffc7a4ff71d8c4865
m.gavrilova <marya.gama1@yandex.ru>
2021-09-27 12:57:20 +0300

    экзамен 5

--- Practice/gavrilova/5.py
+++ Practice/gavrilova/5.py
@@ -1,10 +1,11 @@
-def count_symbol(a):
+def count_symbol(a, s):
     count = 0
-    for i in test_str:
-        if i == 'i':
+    for i in a:
+        if i == s:
             count = count + 1
     return count
 
 test_str = 'Hi, Elvis, I am here!'
-n = count_symbol(test_str)
-print ('Символ i встречается в строке: ' + test_str + ' '+str(n)+' раз')
+simv = 'i'
+n = count_symbol(test_str, simv)
+print ('Символ ' + simv + ' встречается в строке: ' + test_str + ' '+str(n)+' раз')
